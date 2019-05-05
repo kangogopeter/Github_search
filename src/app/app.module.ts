02a643913fd651fa9b403471c616cfdc6ded90dc
kangogopeter <peterkangogo501@gmail.com>
2019-05-05 15:07:26 +0300

    assigning class l for hovering

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubDetailsComponent } from './github-details/github-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/github-details/github-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-details',
+  templateUrl: './github-details.component.html',
+  styleUrls: ['./github-details.component.css']
+})
+export class GithubDetailsComponent implements OnInit {
+  catolog = 'GALLERIES';
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
