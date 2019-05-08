ffbcd7c038ae477d3453ff2dabc5c4e479ced0f8
kangogopeter <peterkangogo501@gmail.com>
2019-05-08 09:58:59 +0300

    addition of boolean  in environment.prod.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://api.github.com/user/kangogopeter?access_token=17f81b17e8f83b169a4f254233f8d9476982e0cf',
   apiSearch: 'https://api.github.com/user/daneden?access_token=17f81b17e8f83b169a4f254233f8d9476982e0cf'
 };
