90bcfaf9fba5813c90e619a93d4e257e3afd74c8
kangogopeter <peterkangogo501@gmail.com>
2019-05-08 10:00:50 +0300

    addition of boolean  in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.github.com/user/kangogopeter?access_token=17f81b17e8f83b169a4f254233f8d9476982e0cf',
+  apiSearch: 'https://api.github.com/user/daneden?access_token=17f81b17e8f83b169a4f254233f8d9476982e0cf'
 };
